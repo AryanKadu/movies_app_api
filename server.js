f5283d0374923a70146d5a5f38f5962bce450461
AryanKadu <aryanpkadu30@gmail.com>
2025-07-27 23:25:29 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const movies = require('./data'); // This imports your exported movies array from data.js
+
+const app = express();
+const PORT = 8080;
+
+// Endpoint to get all movies
+app.get('/api/movies', (req, res) => {
+  res.json(movies);
+});
+
+// (Optional) Endpoint to get a specific movie by ID
+app.get('/api/movies/:id', (req, res) => {
+  const movie = movies.find(m => m.id === req.params.id);
+  if (movie) {
+    res.json(movie);
+  } else {
+    res.status(404).json({ message: 'Movie not found' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
